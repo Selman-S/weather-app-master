95aee489f8aa0d9607369cbc3e3a2b92de158dbe
selman <gunduzgece546@gmail.com>
2022-07-11 01:31:55 +0300

    fahrenheit celcious

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,26 @@
-const loading = document.querySelector('.loading');
-const container = document.querySelector('.container');
+const loading = document.querySelector('.loading')
+const container = document.querySelector('.container')
 const searchBtn = document.querySelector('.search-btn')
-const searchCity = document.querySelector('.search-city');
-const navSearch = document.querySelector('.nav-search');
-const navSrcClose = document.querySelector('.nav-search-close');
+const searchCity = document.querySelector('.search-city')
+const navSearch = document.querySelector('.nav-search')
+const navSrcClose = document.querySelector('.nav-search-close')
 
-const navSearchInput = document.querySelector('.nav-search-input');
-const navSearchButton = document.querySelector('.nav-search-button');
+const navSearchInput = document.querySelector('.nav-search-input')
+const navSearchButton = document.querySelector('.nav-search-button')
 
-
-
-
-
-let units = "metric";
-let apiKey = "00484987152255e2d06f78d9149a1649";
+let units = 'metric'
+let apiKey = '00484987152255e2d06f78d9149a1649'
 // Loading run
 setTimeout(() => {
-  loading.style.display = 'none';
-  container.style.display = 'flex';
-}, 1000);
+  loading.style.display = 'none'
+  container.style.display = 'flex'
+}, 1000)
 
-searchBtn.addEventListener('click',()=> {
-
-
-  searchCity.style.display = 'none';
-  navSearch.style.display = 'flex' ;
-
+searchBtn.addEventListener('click', () => {
+  searchCity.style.display = 'none'
+  navSearch.style.display = 'flex'
 })
-navSrcClose.addEventListener('click',()=>{
+navSrcClose.addEventListener('click', () => {
   searchCity.style.display = 'block'
   navSearch.style.display = 'none'
 })
@@ -35,26 +28,35 @@ navSrcClose.addEventListener('click',()=>{
 
 
 
+document.querySelector('.celci').addEventListener('click', (e)=>{
+ 
+  localStorage.setItem('typeCelc',true)
 
-const getCurrentWeatherData = async (city) => {
+  e.target.classList.add('cel-fa-active')
+  document.querySelector('.fahren').classList.remove('cel-fa-active')
+})
+document.querySelector('.fahren').addEventListener('click', (e)=>{
+    typeCelc = false 
+    e.target.classList.add('cel-fa-active')
+    document.querySelector('.celci').classList.remove('cel-fa-active')
+  })
 
+const getCurrentWeatherData = async city => {
+  let lang = 'tr'
 
-let lang = "tr";
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=1723148a77444853837f03372fba544a&days=6`
+  try {
+    const response = await axios(url)
 
-let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=1723148a77444853837f03372fba544a&days=6`;
-try {
-  const response = await axios(url)
+    const { data, country_code, city_name } = response.data
 
-  
-  const {data,country_code,city_name} = response.data
-
-
-if (typeof data=='undefined') {
-  document.querySelector('.valid-city').style.display= 'block';
-  navSearchInput.value =''
-
-}else {
-  document.querySelector('.nav-search-result-ul').innerHTML += `<li class="nav-search-result-li btnn" onclick= "setWeatherData()">
+    if (typeof data == 'undefined') {
+      document.querySelector('.valid-city').style.display = 'block'
+      navSearchInput.value = ''
+    } else {
+      document.querySelector(
+        '.nav-search-result-ul'
+      ).innerHTML += `<li class="nav-search-result-li btnn" onclick= "setWeatherData()">
   <button class="nav-search-result-btn " ><span class="nav-search-result-spn">${city_name}</span>,<span> ${country_code}</span></button><svg
     stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 384 512" class="icon" height="1em"
     width="1em" xmlns="http://www.w3.org/2000/svg">
@@ -63,51 +65,67 @@ if (typeof data=='undefined') {
     </path>
   </svg>
 </li>`
-}
+    }
 
-  let iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+    let iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
 
-  return   
-} catch (err) {
-  return err
-}
-}
-
-navSearchButton.addEventListener('click', () =>{
- if (navSearchInput.value) {
-
-  let city = navSearchInput.value
-  getCurrentWeatherData(city)
+    return
+  } catch (err) {
+    return err
+  }
 }
 
 
+
+navSearchButton.addEventListener('click', () => {
+  if (navSearchInput.value) {
+    let city = navSearchInput.value
+    getCurrentWeatherData(city)
+  }
 })
- // datayı çek yerleştir
+// datayı çek yerleştir
 
- const setWeatherData = async () => {
- const citY = document.querySelector('.nav-search-result-spn').textContent
+const setWeatherData = async () => {
+  const citY = document.querySelector('.nav-search-result-spn').textContent
 
-   let lang = "tr";
-   
-   let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${citY}&key=1723148a77444853837f03372fba544a&days=6`;
-   try {
-     const response = await axios(url)
-     const {data,country_code,city_name} = response.data
-   
-     document.querySelector('.nav-search-result-ul').innerHTML =''
-     navSearchInput.value ='';
-     searchCity.style.display = 'block'
-  navSearch.style.display = 'none'
-  // fahrenayt celcious
-    let x =1
-    document.querySelector('.celci-s').innerText ='°C'
+  let lang = 'tr'
+
+  let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${citY}&key=1723148a77444853837f03372fba544a&days=6`
+  try {
+    const response = await axios(url)
+    const { data, country_code, city_name } = response.data
+
+    document.querySelector('.nav-search-result-ul').innerHTML = ''
+    navSearchInput.value = ''
+    searchCity.style.display = 'block'
+    navSearch.style.display = 'none'
+
+    const { wind_cdir, temp, weather, datetime, valid_date } = data[0]
+    // fahrenayt celcious
     
-    const {wind_cdir,temp,weather}= data[0]
+  
+
+
+    if (typeCelc) {
+   
+      document.querySelector('.celci-s').innerText = '°C'
+ document.querySelector('.degree-s span').innerText = String(temp ).split(
+      '.'
+    )[0]
+    }else {
+
+
+      document.querySelector('.celci-s').innerText = '°F'
+ document.querySelector('.degree-s span').innerText = String((temp * 1.8)+ 32.).split(
+      '.'
+    )[0]
+    }
+
     
+
     // weather icons
-    let iconUrl;
- 
-    
+    let iconUrl
+
     switch (weather.code) {
       case 201:
       case 202:
@@ -116,84 +134,114 @@ navSearchButton.addEventListener('click', () =>{
       case 231:
       case 232:
       case 233:
-      
-        iconUrl ='./assets/img/Thunderstorm.png'
-        break;
+        iconUrl = './assets/img/Thunderstorm.png'
+        break
       case 300:
       case 301:
       case 302:
       case 511:
       case 611:
       case 612:
-        iconUrl ='./assets/img/Sleet.png'
-        break;
+        iconUrl = './assets/img/Sleet.png'
+        break
       case 500:
       case 501:
-
-   
-
-        iconUrl ='./assets/img/LightRain.png'
-        break;
+        iconUrl = './assets/img/LightRain.png'
+        break
       case 502:
       case 522:
-
-        iconUrl ='./assets/img/HeavyRain.png'
-        break;  
+        iconUrl = './assets/img/HeavyRain.png'
+        break
       case 601:
       case 621:
-
-
-        iconUrl ='./assets/img/Snow.png'
-        break;
+        iconUrl = './assets/img/Snow.png'
+        break
       case 622:
       case 623:
-        iconUrl ='./assets/img/Hail.png'
-        break;
+        iconUrl = './assets/img/Hail.png'
+        break
       case 521:
       case 520:
       case 522:
-        iconUrl ='./assets/img/Shower.png'
-        break; 
-        case 800:
-        iconUrl ='./assets/img/Clear.png'
-        break;
-        case 801:
-        case 802:
-        case 700:
-        case 711:
-        case 721:
-        case 731:
-        case 741:
-        case 751:
-        iconUrl ='./assets/img/LightCloud.png'
-        break;
+        iconUrl = './assets/img/Shower.png'
+        break
+      case 800:
+        iconUrl = './assets/img/Clear.png'
+        break
+      case 801:
+      case 802:
+      case 700:
+      case 711:
+      case 721:
+      case 731:
+      case 741:
+      case 751:
+        iconUrl = './assets/img/LightCloud.png'
+        break
       case 803:
       case 804:
       case 900:
-        iconUrl ='./assets/img/HeavyCloud.png'
+        iconUrl = './assets/img/HeavyCloud.png'
     }
-     console.log(wind_cdir);
-     
-     // set data first page
+    console.log(wind_cdir)
+
+    // set data first page
     //celcious
-     document.querySelector('.degree-s span').innerText = String(temp*x).split('.')[0];
+   
     // weather desc
-    document.querySelector('.w-desc').innerText =weather.description;
+    document.querySelector('.w-desc').innerText = weather.description
 
     // img icon
-    document.querySelector('.w-img').src = iconUrl;
+    document.querySelector('.w-img').src = iconUrl
+    // city name text
+    document.querySelector('.city-name').innerText = city_name
+    document.querySelector('.country-name').innerText = country_code
+
+    // date
+    const launchDate = new Date('2022-07-19')
+    const futureDate = new Date()
+    futureDate.setTime(launchDate.getTime())
+    const newDate = launchDate.toString().split(' ')
+    console.log(launchDate.toString().split(' '))
+
+    document.querySelector('.date-day').innerText = newDate[0]
+    document.querySelector('.date-num').innerText = newDate[2]
+    document.querySelector('.date-month').innerText = newDate[1];
+
+
+ 
 
 
 
-   }
-   catch(err){ 
-    document.querySelector('.enter-city').style.display='block';
-      document.querySelector('.enter-city').innerText = `${err.message}`
-    setTimeout(() =>{
-      navSearchInput.value ='';
-      document.querySelector('.enter-city').style.display='none';
-    },5000)
-    
-   
-     
-   }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  } catch (err) {
+    document.querySelector('.enter-city').style.display = 'block'
+    document.querySelector('.enter-city').innerText = `${err.message}`
+    setTimeout(() => {
+      navSearchInput.value = ''
+      document.querySelector('.enter-city').style.display = 'none'
+    }, 5000)
+  }
+}
